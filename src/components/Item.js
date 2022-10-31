a276914eec4e8d6afded618e85a0cb9a50ac3f2b
Andrew Wang <Andrewruujin@gmail.com>
2022-10-31 19:02:37 -0400

    commit tree 979e266ba7c0c7530d0e3e903414572e3ba85d1f

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 function Item({ name, category }) {
+
+  const [isInCart, setIsOn] = useState(false);
+  const inCartBtnText = isInCart ? "Remove From Cart" : "Add to Cart"
+  const inCartList = isInCart ? "in-cart" : ""
+  const inCartBtn = isInCart ? "remove" : "add"
+
+  function handleClick(){
+    setIsOn((isInCart) => !isInCart);
+  }
   return (
-    <li className="">
+    <li className={inCartList}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCartBtn} onClick={handleClick}>{inCartBtnText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  /* const [shoppingList, setShopplingList] = useState(Item) */
+  const [filter, setFilter] = useState("All")
+
+  function handleChange(event){
+    setFilter(event.target.value)
+  }
+
+  const displayItem = items.filter((item)=>{
+    if(filter === "All"){
+      return true
+    }else{
+      return item.category === filter
+    }
+  })
+
   return (
-    <div className="ShoppingList">
+    <div className="ShoppingList" onChange={handleChange}>
       <div className="Filter">
         <select name="filter">
           <option value="All">Filter by category</option>
@@ -13,7 +28,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {displayItem.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
